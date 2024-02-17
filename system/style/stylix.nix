9aaf26f08b3d4ad319f2f9811957fa0659ee9b1c
Jonathan Walker <w@lker.dev>
2024-02-17 09:51:05 +0900

    split font types

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -19,16 +19,16 @@ in
   stylix.base16Scheme = ./. + themePath;
   stylix.fonts = {
     monospace = {
-      name = userSettings.font;
-      package = userSettings.fontPkg;
+      name = userSettings.monoFont;
+      package = userSettings.monoFontPkg;
     };
     serif = {
-      name = userSettings.font;
-      package = userSettings.fontPkg;
+      name = userSettings.serifFont;
+      package = userSettings.serifFontPkg;
     };
     sansSerif = {
-      name = userSettings.font;
-      package = userSettings.fontPkg;
+      name = userSettings.sansSerifFont;
+      package = userSettings.sansSerifFontPkg;
     };
     emoji = {
       name = "Noto Color Emoji";
