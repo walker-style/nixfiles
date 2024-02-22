1add24cb132ccffcc229a327a6f1f8c89a301670
Jonathan Walker <w@lker.dev>
2024-02-22 11:49:05 +0900

    zshrc into home-manager

--- profiles/developer/home.nix
+++ profiles/developer/home.nix
@@ -89,6 +89,7 @@
 
     ".config/i3/config".source = ../../users/dotfiles/i3/config;
     ".config/tmux/tmux.conf".source = ../../users/dotfiles/tmux/tmux.conf;
+    ".zshrc".source = ../../user/dotfiles/zsh/zshrc;
   };
 
   # Home Manager can also manage your environment variables through
